69073b6fa6ef7762fc05bb9ec1c4f97cc11734ee
DESTINY GROWRICH <destinyofowochogrowrich@gmail.com>
2026-08-23 11:04:50 +0100

    feat: add LoreLab landing page

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>LORELAB</h1>
+      <p>Find the story. Cook the story.</p>
+      <button type="button">LET&apos;S COOK 🔥</button>
+    </main>
+  )
+}
